b93af97ff836f9a500b16d50c89bb6f66fb58d9b
tmaulon <thomas.maulon@gmail.com>
2019-10-11 11:47:09 +0200

    Create a click action on an item in our FlatList

--- Components/Search.js
+++ Components/Search.js
@@ -78,6 +78,10 @@ class Search extends React.Component {
     );
   }
 
+  _displayDetailForFilm = idFilm => {
+    console.log(`Display film with id ${idFilm}`);
+  };
+
   render() {
     console.log(this.state.isLoading);
     return (
@@ -92,7 +96,12 @@ class Search extends React.Component {
         <FlatList
           data={this.state.films}
           keyExtractor={item => item.id.toString()}
-          renderItem={({ item }) => <FilmItem film={item} />}
+          renderItem={({ item }) => (
+            <FilmItem
+              film={item}
+              displayDetailForFilm={this._displayDetailForFilm}
+            />
+          )}
           onEndReachedThreshold={0.5}
           onEndReached={() => {
             if (this.page < this.totalPages) {
--- Navigation/Navigation.js
+++ Navigation/Navigation.js
@@ -3,14 +3,18 @@
 import { createStackNavigator } from "react-navigation-stack";
 import { createAppContainer } from "react-navigation";
 import Search from "../Components/Search";
+import FilmDetail from "../Components/FilmDetail";
 
 const SearchStackNavigator = createStackNavigator({
   Search: {
-    // Ici j'ai appelé la vue "Search" mais on peut mettre ce que l'on veut. C'est le nom qu'on utilisera pour appeler cette vue
     screen: Search,
     navigationOptions: {
       title: "Rechercher"
     }
+  },
+
+  FilmDetail: {
+    screen: FilmDetail
   }
 });
 
