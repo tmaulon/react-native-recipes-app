f5c32134f0b3fbfdcc811de3b729a5190adb76f9
tmaulon <thomas.maulon@gmail.com>
2019-10-11 14:56:18 +0200

    create new function to get detail for a film

--- API/TMDBApi.js
+++ API/TMDBApi.js
@@ -14,6 +14,19 @@ export function getFilmsFromApiWithSearchedText(text, page) {
     .catch(error => console.error(error));
 }
 
+export function getFilmDetailFromApi(id) {
+  const url =
+    "https://api.themoviedb.org/3/movie/" +
+    id +
+    "?api_key=" +
+    API_TOKEN +
+    "&language=fr";
+
+  return fetch(url)
+    .then(response => response.json())
+    .catch(error => console.error(error));
+}
+
 // API/TMDBApi.js
 
 export function getImageFromApi(name) {
