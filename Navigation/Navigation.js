7bb5bb97fedd8f75a8c88741682167131e90972d
tmaulon <thomas.maulon@gmail.com>
2019-10-13 17:30:37 +0200

    add Favorites screen in Navigation and refactor Search + custom tabBar

--- Navigation/Navigation.js
+++ Navigation/Navigation.js
@@ -1,9 +1,13 @@
 // Navigation/Navigation.js
 
+import React from "react";
+import { StyleSheet, Image } from "react-native";
 import { createStackNavigator } from "react-navigation-stack";
 import { createAppContainer } from "react-navigation";
+import { createBottomTabNavigator } from "react-navigation-tabs";
 import Search from "../Components/Search";
 import FilmDetail from "../Components/FilmDetail";
+import Favorites from "../Components/Favorites";
 
 const SearchStackNavigator = createStackNavigator({
   Search: {
@@ -18,4 +22,50 @@ const SearchStackNavigator = createStackNavigator({
   }
 });
 
-export default createAppContainer(SearchStackNavigator);
+const MoviesTabNavigator = createBottomTabNavigator(
+  {
+    Search: {
+      screen: SearchStackNavigator,
+      navigationOptions: {
+        tabBarIcon: () => {
+          return (
+            <Image
+              source={require("../Images/ic_search.png")}
+              style={styles.icon}
+            />
+          );
+        }
+      }
+    },
+    Favorites: {
+      screen: Favorites,
+      navigationOptions: {
+        tabBarIcon: () => {
+          return (
+            <Image
+              source={require("../Images/ic_favorite.png")}
+              style={styles.icon}
+            />
+          );
+        }
+      }
+    }
+  },
+  {
+    tabBarOptions: {
+      activeBackgroundColor: "#ddd",
+      inactiveBackgroundColor: "#fff",
+      showLabel: false,
+      showIcon: true
+    }
+  }
+);
+
+const styles = StyleSheet.create({
+  icon: {
+    width: 30,
+    height: 30
+  }
+});
+
+export default createAppContainer(MoviesTabNavigator);
