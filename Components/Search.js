11ed7cf12c1d60a2f51b5fe6042bbeeccb9553e4
Maulon <tmaulon@betomorrow.com>
2020-01-04 12:53:33 +0100

    improve load more on endReached

--- Components/Search.js
+++ Components/Search.js
@@ -7,10 +7,20 @@ import {
   TextInput,
   Button,
   ActivityIndicator,
-  SafeAreaView
+  SafeAreaView,
+  FlatList
 } from "react-native";
 import RecipesList from "./RecipesList";
+import RecipeItem from "./RecipeItem";
 import { getRecipesFromApiWithSearchedText } from "../API/RecipeSearchAPi";
+import FontAwesomeIcon from "react-native-vector-icons/FontAwesome";
+import {
+  widthPercentageToDP as wp,
+  heightPercentageToDP as hp
+} from "react-native-responsive-screen";
+import AntDesignIcon from "react-native-vector-icons/AntDesign";
+
+import { colors } from "../Helpers/Colors";
 
 class Search extends React.Component {
   constructor(props) {
@@ -30,7 +40,11 @@ class Search extends React.Component {
     if (this.searchedText.length > 0) {
       this.setState({ isLoading: true });
       console.log(this.searchedText);
-      getRecipesFromApiWithSearchedText(this.searchedText).then(data => {
+      getRecipesFromApiWithSearchedText(
+        this.searchedText,
+        this.minPageNumber,
+        this.lastPageNumber + 10
+      ).then(data => {
         this.minPageNumber = data.from;
         this.lastPageNumber = data.to;
         this.totalPages = data.count;
@@ -39,6 +53,38 @@ class Search extends React.Component {
           isLoading: false
         });
       });
+    }
+  }
+  _loadMoreRecipes() {
+    console.log(
+      "end reachend, lastpagenumber, recipes : ",
+      this.lastPageNumber,
+      this.state.recipes
+    );
+
+    if (this.searchedText.length > 0) {
+      this.setState({ isLoading: true });
+      this.minPageNumber = this.lastPageNumber;
+      this.lastPageNumber = this.lastPageNumber + 20;
+      getRecipesFromApiWithSearchedText(
+        this.searchedText,
+        this.minPageNumber,
+        this.lastPageNumber
+      ).then(data => {
+        // this.minPageNumber = data.from;
+        // this.lastPageNumber = data.to;
+        // this.totalPages = data.count;
+        this.setState({
+          recipes: [...this.state.recipes, ...data.hits],
+          isLoading: false
+        });
+      });
+      console.log(
+        "new minPageNumber, new lastpagenumber, new recipes : ",
+        this.minPageNumber,
+        this.lastPageNumber,
+        this.state.recipes
+      );
     }
   }
 
@@ -61,6 +107,7 @@ class Search extends React.Component {
   }
 
   _displayDetailForRecipe = uri => {
+    console.log("display recipe with uri : ", uri);
     this.props.navigation.navigate("RecipeDetail", { uri: uri });
   };
 
@@ -77,15 +124,52 @@ class Search extends React.Component {
   render() {
     return (
       <SafeAreaView style={styles.main_container}>
-        <View style={styles.main_container}>
+        {/* <View style={styles.main_container}> */}
+        <View style={styles.textinput_wrapper}>
+          {/* <SearchIcon fontSize={30} style={{ color: "#40B89F" }} /> */}
+          <FontAwesomeIcon
+            name="search"
+            fontSize={30}
+            color={colors.turquoiseGreen}
+          />
           <TextInput
             style={styles.textinput}
             placeholder="Nom d'un ingrédient"
             onChangeText={text => this._searchTextInputChanged(text)}
             onSubmitEditing={() => this._searchRecipes()}
           />
-          <Button title="Rechercher" onPress={() => this._searchRecipes()} />
-          <RecipesList
+          {/* <Button title="Rechercher" onPress={() => this._searchRecipes()} /> */}
+        </View>
+        <FlatList
+          style={{ flex: 1, width: "100%", paddingTop: hp("13%") }}
+          data={this.state.recipes}
+          // extraData={this.props.favoritesRecipe}
+          keyExtractor={(item, index) => `${item.uri}-${index}`}
+          renderItem={({ item }) => (
+            <RecipeItem
+              recipe={item.recipe}
+              // isRecipeFavorite={
+              //   this.props.favoritesRecipe.findIndex(
+              //     recipe => recipe.uri === item.recipe.uri
+              //   ) !== -1
+              //     ? true
+              //     : false
+              // }
+              // Bonus pour différencier les recipes déjà présent dans notre state global et qui n'ont donc pas besoin d'être récupérés depuis l'API
+              displayDetailForRecipe={this._displayDetailForRecipe}
+            />
+          )}
+          onEndReachedThreshold={0.5}
+          onEndReached={() => {
+            // if (
+            // !this.props.favoriteList &&
+            // this.props.lastPageNumber < this.props.totalPages
+            // ) {
+            this._loadMoreRecipes();
+            // }
+          }}
+        />
+        {/* <RecipesList
             recipes={this.state.recipes}
             navigation={this.props.navigation}
             loadrecipes={this._loadRecipes}
@@ -93,9 +177,9 @@ class Search extends React.Component {
             lastPageNumber={this.lastPageNumber}
             totalPages={this.totalPages}
             favoriteList={false}
-          />
-          {this._displayLoading()}
-        </View>
+          /> */}
+        {this._displayLoading()}
+        {/* </View> */}
       </SafeAreaView>
     );
   }
@@ -103,14 +187,36 @@ class Search extends React.Component {
 
 const styles = StyleSheet.create({
   main_container: {
-    flex: 1
+    flex: 1,
+    position: "relative",
+    alignItems: "center"
+  },
+  textinput_wrapper: {
+    zIndex: 10,
+    width: wp("95%"),
+    position: "absolute",
+    top: 0,
+    flexDirection: "row",
+    alignItems: "center",
+    marginHorizontal: 15,
+    marginTop: 15,
+    backgroundColor: colors.white,
+    borderRadius: 35,
+    paddingHorizontal: 25,
+    paddingVertical: 5,
+    elevation: 10,
+    shadowColor: colors.black,
+    shadowOffset: { width: 20, height: 20 },
+    shadowOpacity: 0.15,
+    shadowRadius: 10
   },
   textinput: {
     marginLeft: 5,
     marginRight: 5,
     height: 50,
-    borderColor: "#000000",
-    borderWidth: 1,
+    width: "100%",
+    // borderColor: "#000000",
+    // borderWidth: 1,
     paddingLeft: 5
   },
   loading_container: {
