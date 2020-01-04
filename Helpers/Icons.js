070676d57a2d700b6033c2999ac22e8a02f3171a
Maulon <tmaulon@betomorrow.com>
2020-01-04 12:52:43 +0100

    create some helpers reminders

---
+++ Helpers/Icons.js
@@ -0,0 +1,37 @@
+import React from "react";
+import FontAwesomeIcon from "react-native-vector-icons/FontAwesome";
+import AntDesignIcon from "react-native-vector-icons/AntDesign";
+
+export const CalendarIcon = (size, color) => (
+  <FontAwesomeIcon name="calendar" fontSize={size} color={color} />
+);
+export const CloseCircleIcon = (size, color) => (
+  <AntDesignIcon name="closecircle" size={size} color={color} />
+);
+export const EmptyCloseCircleIcon = (size, color) => (
+  <AntDesignIcon name="closecircleo" size={size} color={color} />
+);
+export const EmptyHeartIcon = (size, color) => (
+  <FontAwesomeIcon name="heart-o" fontSize={size} color={color} />
+);
+export const EmptyUserCircleIcon = (size, color) => (
+  <FontAwesomeIcon name="user-circle-o" fontSize={size} color={color} />
+);
+export const FilterIcon = (size, color) => (
+  <FontAwesomeIcon name="filter" size={size} color={color} />
+);
+export const HeartIcon = (size, color) => (
+  <FontAwesomeIcon name="heart" fontSize={size} color={color} />
+);
+export const RocketIcon = (size, color) => (
+  <FontAwesomeIcon name="rocket" fontSize={size} color={color} />
+);
+export const SearchIcon = (size, color) => (
+  <FontAwesomeIcon name="search" fontSize={size} color={color} />
+);
+export const ShoppingBasketIcon = (size, color) => (
+  <FontAwesomeIcon name="shopping-basket" fontSize={size} color={color} />
+);
+export const UserCircleIcon = (size, color) => (
+  <FontAwesomeIcon name="user-circle" fontSize={size} color={color} />
+);
