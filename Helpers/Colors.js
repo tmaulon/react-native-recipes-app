d2d795052013c30eaca043e9178769048a40211e
Maulon <tmaulon@betomorrow.com>
2020-01-04 12:52:59 +0100

    create a colors helper

---
+++ Helpers/Colors.js
@@ -0,0 +1,11 @@
+export const colors = {
+  lightGreen: "#58FFC0",
+  turquoiseGreen: "#40B89F",
+  lightRed: "#FF4B46",
+  darkRed: "#CC0000",
+  red: "#FF0000",
+  lightGrey: "#ededed",
+  grey: "#dedede",
+  black: "#0F0F0F",
+  white: "#fff"
+};
