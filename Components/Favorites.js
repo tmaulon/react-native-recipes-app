910de7c8a8fbb7db2a547a6fd3d986a0b0b453e4
Maulon <tmaulon@betomorrow.com>
2020-01-05 21:09:51 +0100

    add safe area view in favorites

--- Components/Favorites.js
+++ Components/Favorites.js
@@ -1,24 +1,24 @@
 // Components/Favorites.js
 
 import React from "react";
-import { StyleSheet, Text, View } from "react-native";
+import { StyleSheet, Text, View, SafeAreaView } from "react-native";
 import RecipesList from "./RecipesList";
 import { connect } from "react-redux";
 
 class Favorites extends React.Component {
   render() {
     return (
-      <View>
+      <SafeAreaView>
         <Text>Favorites recipes</Text>
         {/* {this.props.favoritesRecipe.map(r => (
           <Text>{r.label}</Text>
         ))} */}
-        <RecipesList
+        {/* <RecipesList
           recipes={this.props.favoritesRecipe}
           navigation={this.props.navigation}
           favoriteList={true} // Ici on est bien dans le cas de la liste des recipes favoris. Ce booléen à true permettra d'empêcher de lancer la recherche de plus de recipes après un scroll lorsqu'on est sur la vue Favoris.
-        />
-      </View>
+        /> */}
+      </SafeAreaView>
     );
   }
 }
