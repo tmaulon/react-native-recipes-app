c7554a5197c0d292f2edf4447474ee5f26d36b44
Maulon <tmaulon@betomorrow.com>
2020-01-05 22:34:55 +0100

    improve favorites screen

--- Components/Favorites.js
+++ Components/Favorites.js
@@ -1,29 +1,64 @@
-// Components/Favorites.js
-
 import React from "react";
-import { StyleSheet, Text, View, SafeAreaView } from "react-native";
-import RecipesList from "./RecipesList";
+import {
+  StyleSheet,
+  Text,
+  View,
+  SafeAreaView,
+  Image,
+  TouchableOpacity
+} from "react-native";
+import FadeIn from "../Animations/FadeIn";
 import { connect } from "react-redux";
+import { Dimensions } from "react-native";
+const screenWidth = Dimensions.get("window").width;
+const screenHeight = Dimensions.get("window").height;
+import {
+  widthPercentageToDP as wp,
+  heightPercentageToDP as hp
+} from "react-native-responsive-screen";
+import FontAwesomeIcon from "react-native-vector-icons/FontAwesome";
+import MaterialCommunityIcon from "react-native-vector-icons/MaterialCommunityIcons";
+import FontAwesome5Icon from "react-native-vector-icons/FontAwesome5";
+import { colors } from "../Helpers/Colors";
+import { ScrollView } from "react-native-gesture-handler";
+import RecipesList from "./RecipesList";
 
 class Favorites extends React.Component {
+  _displayFavoriteImage() {
+    console.log("in _display favorite image : ", this.props.favoritesRecipe);
+
+    return this.props.isFavoriteRecipe ? (
+      <FontAwesomeIcon
+        name="heart"
+        fontSize={35}
+        size={35}
+        color={colors.white}
+      />
+    ) : (
+      <FontAwesomeIcon
+        name="heart-o"
+        fontSize={35}
+        size={35}
+        color={colors.white}
+      />
+    );
+  }
+
   render() {
+    console.log("in favorites, this.props", this.props);
+
     return (
-      <SafeAreaView>
-        <Text>Favorites recipes</Text>
-        {/* {this.props.favoritesRecipe.map(r => (
-          <Text>{r.label}</Text>
-        ))} */}
-        {/* <RecipesList
+      <SafeAreaView style={{ flex: 1 }}>
+        <RecipesList
+          style={{ flex: 1 }}
           recipes={this.props.favoritesRecipe}
           navigation={this.props.navigation}
-          favoriteList={true} // Ici on est bien dans le cas de la liste des recipes favoris. Ce booléen à true permettra d'empêcher de lancer la recherche de plus de recipes après un scroll lorsqu'on est sur la vue Favoris.
-        /> */}
+          favoriteList={true}
+        />
       </SafeAreaView>
     );
   }
 }
-
-const styles = StyleSheet.create({});
 
 const mapStateToProps = state => {
   return {
@@ -31,4 +66,5 @@ const mapStateToProps = state => {
   };
 };
 
+// export default Favorites;
 export default connect(mapStateToProps)(Favorites);
