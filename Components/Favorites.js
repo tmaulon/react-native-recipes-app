0ff19f87eeab9378905ccc96562cdd7a2fd11408
Maulon <tmaulon@betomorrow.com>
2020-01-05 22:36:35 +0100

    remove useless imports

--- Components/Favorites.js
+++ Components/Favorites.js
@@ -1,26 +1,8 @@
 import React from "react";
-import {
-  StyleSheet,
-  Text,
-  View,
-  SafeAreaView,
-  Image,
-  TouchableOpacity
-} from "react-native";
-import FadeIn from "../Animations/FadeIn";
+import { SafeAreaView } from "react-native";
 import { connect } from "react-redux";
-import { Dimensions } from "react-native";
-const screenWidth = Dimensions.get("window").width;
-const screenHeight = Dimensions.get("window").height;
-import {
-  widthPercentageToDP as wp,
-  heightPercentageToDP as hp
-} from "react-native-responsive-screen";
 import FontAwesomeIcon from "react-native-vector-icons/FontAwesome";
-import MaterialCommunityIcon from "react-native-vector-icons/MaterialCommunityIcons";
-import FontAwesome5Icon from "react-native-vector-icons/FontAwesome5";
 import { colors } from "../Helpers/Colors";
-import { ScrollView } from "react-native-gesture-handler";
 import RecipesList from "./RecipesList";
 
 class Favorites extends React.Component {
