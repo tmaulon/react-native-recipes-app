9c03c3880d3083c769aeafd55697dd76d8ff6419
Maulon <tmaulon@betomorrow.com>
2020-01-05 21:08:40 +0100

    create recipesList

--- Components/RecipesList.js
+++ Components/RecipesList.js
@@ -4,6 +4,13 @@ import React from "react";
 import { StyleSheet, FlatList, Text, View } from "react-native";
 import RecipeCard from "./RecipeCard";
 import { connect } from "react-redux";
+import {
+  widthPercentageToDP as wp,
+  heightPercentageToDP as hp
+} from "react-native-responsive-screen";
+import { Dimensions } from "react-native";
+const screenWidth = Dimensions.get("window").width;
+const screenHeight = Dimensions.get("window").height;
 
 class RecipesList extends React.Component {
   constructor(props) {
@@ -14,49 +21,52 @@ class RecipesList extends React.Component {
   }
 
   _displayDetailForRecipe = uri => {
-    this.props.navigation.navigate("RecipeDetail");
+    console.log("display recipe with uri : ", uri);
+    this.props.navigation.navigate("RecipeDetail", { uri: uri });
   };
 
   render() {
     return (
       <FlatList
-        style={styles.list}
+        style={{
+          flex: 1,
+          height: "100%",
+          width: screenWidth,
+          paddingTop: hp("13%")
+        }}
         data={this.props.recipes}
         extraData={this.props.favoritesRecipe}
         keyExtractor={(item, index) => `${item.uri}-${index}`}
-        renderItem={({ item }) => (
-          <RecipeCard
-            recipe={item.recipe}
-            isRecipeFavorite={
-              this.props.favoritesRecipe.findIndex(
-                recipe => recipe.uri === item.recipe.uri
-              ) !== -1
-                ? true
-                : false
-            } // Bonus pour différencier les recipes déjà présent dans notre state global et qui n'ont donc pas besoin d'être récupérés depuis l'API
-            displayDetailForRecipe={this._displayDetailForRecipe}
-          />
-        )}
+        renderItem={({ item }) => {
+          const { recipe } = item;
+          console.log(recipe);
+          return (
+            <RecipeCard
+              recipe={recipe}
+              isFavoriteRecipe={
+                this.props.favoritesRecipe.findIndex(
+                  recipe => recipe.uri === item.recipe.uri
+                ) !== -1
+                  ? true
+                  : false
+              }
+              displayDetailForRecipe={this._displayDetailForRecipe}
+            />
+          );
+        }}
         onEndReachedThreshold={0.5}
         onEndReached={() => {
-          if (
-            !this.props.favoriteList &&
-            this.props.lastPageNumber < this.props.totalPages
-          ) {
-            this.props.loadrecipes();
-          }
+          // if (
+          // !this.props.favoriteList &&
+          // this.props.lastPageNumber < this.props.totalPages
+          // ) {
+          this.props.loadMoreRecipes();
+          // }
         }}
       />
     );
   }
 }
-
-const styles = StyleSheet.create({
-  list: {
-    flex: 1
-  }
-});
-
 const mapStateToProps = state => {
   return {
     favoritesRecipe: state.favoritesRecipe
